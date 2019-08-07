8c2e5f191b68ed37218299ae09b4b5c773b2be4d
Pål Grønås Drange <PGDR@equinor.com>
2019-08-07 23:26:03 +0200

    Add url

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from skbuild import setup
 
 setup(
     name="minisat",
-    packages=['minisat'],
-    version='0.0.1',
+    packages=["minisat"],
+    version="0.0.1",
+    author="pgdr",
+    url="https://github.com/pgdr/python-minisat",
 )
